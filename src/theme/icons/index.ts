59216c0afda836b338b66dcddbb6f55db7b5ce51
Arsène DOBROVOLSKYY <arsened.devweb@gmail.com>
2024-08-27 10:36:37 +0200

    Add icons

---
+++ src/theme/icons/index.ts
@@ -0,0 +1,17 @@
+import arrowDown from './arrowDown.png';
+import arrowLeft from './arrowLeft.png';
+import arrowRight from './arrowRight.png';
+import arrowUp from './arrowUp.png';
+import horn from './horn.png';
+import videoOff from './videoOff.png';
+import videoOn from './videoOn.png';
+
+export const icons = {
+  arrowDown,
+  arrowLeft,
+  arrowRight,
+  arrowUp,
+  horn,
+  videoOff,
+  videoOn,
+};
